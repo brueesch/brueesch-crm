5d7b1c0c43d319f674173ac8061ea1ded1175111
Christian <c.brueesch@yahoo.de>
2016-03-13 22:15:22 +0100

    Initial Commit Startfiles of Springboot

---
+++ src/main/java/ch/brueesch/crm/BrueeschCrmApplication.java
@@ -0,0 +1,12 @@
+package ch.brueesch.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrueeschCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrueeschCrmApplication.class, args);
+	}
+}
