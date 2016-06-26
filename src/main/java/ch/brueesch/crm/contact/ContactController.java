625415ee86be9451b374bbabedc587ba67aab6b2
Christian Brüesch <christian.brueesch@confinale.ch>
2016-06-27 00:25:35 +0200

    Opportunity and Contacts Stuff

--- src/main/java/ch/brueesch/crm/contact/ContactController.java
+++ src/main/java/ch/brueesch/crm/contact/ContactController.java
@@ -1,7 +1,7 @@
-package ch.brueesch.crm.opportunity;
+package ch.brueesch.crm.contact;
 
 import ch.brueesch.crm.company.CompanyRepository;
-import ch.brueesch.crm.contact.ContactRepository;
+import ch.brueesch.crm.opportunity.OpportunityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,24 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class OpportunityController {
+public class ContactController {
 
     private final CompanyRepository companyRepository;
     private final ContactRepository contactRepository;
     private final OpportunityRepository opportunityRepository;
 
     @Autowired
-    public OpportunityController(CompanyRepository companyRepository, ContactRepository contactRepository, OpportunityRepository opportunityRepository) {
+    public ContactController(CompanyRepository companyRepository, ContactRepository contactRepository, OpportunityRepository opportunityRepository) {
         this.companyRepository = companyRepository;
         this.contactRepository = contactRepository;
         this.opportunityRepository = opportunityRepository;
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/opportunity/{id}")
-    public String renderOpportunity(Model model, @PathVariable Long id) {
-        model.addAttribute("contacts", contactRepository.findAll());
+    @RequestMapping(method = RequestMethod.GET, value = "/contact/{id}")
+    public String renderContact(Model model, @PathVariable Long id) {
+        model.addAttribute("contact", contactRepository.getOne(id));
         model.addAttribute("companies", companyRepository.findAll());
-        model.addAttribute("opportunity", opportunityRepository.getOne(id));
-        return "opportunity";
+        model.addAttribute("opportunities", opportunityRepository.findAll());
+        return "contact";
     }
 }
